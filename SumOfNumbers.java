279b1d303e5faccf2094d4086e9407b373e1a137
Karthik Kumar Bandaru <bandarukarthik24@gmail.com>
2025-12-31 16:50:23 +0530

    Added program to calculate sum of two numbers

---
+++ SumOfNumbers.java
@@ -0,0 +1,9 @@
+public class SumOfNumbers {
+    public static void main(String[] args) {
+        int sum=0;
+        int a=10;
+        int b=20;
+        sum=a+b;
+        System.out.println("sum of two numbers is: " + sum);
+    }
+}
