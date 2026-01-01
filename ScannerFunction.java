63a78911ee3af6e94b88dad74517b49e9a00fc13
Karthik Kumar Bandaru <bandarukarthik24@gmail.com>
2026-01-01 20:21:03 +0530

    Added Scanner Class with Exception handling

--- ScannerFunction.java
+++ ScannerFunction.java
@@ -1,9 +1,13 @@
 import java.util.Scanner;
 public class ScannerFunction {
     public static void main(String args[]){
+        try{
         Scanner sc= new Scanner(System.in);
         System.out.print("Enter your name: ");
         String name=sc.nextLine();
         System.out.println("Hello, "+name+"!");
+        sc.close();
+        }catch(Exception e){
+            System.out.println("An error occurred: " + e.getMessage());
     }
 }
