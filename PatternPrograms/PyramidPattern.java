6171589d87bdd9690bc2c8a118602c8ea523973b
Karthik Kumar Bandaru <bandarukarthik24@gmail.com>
2026-01-03 12:35:58 +0530

    Pyramid pattern program using java.

---
+++ PatternPrograms/PyramidPattern.java
@@ -0,0 +1,19 @@
+package PatternPrograms;
+
+public class PyramidPattern {
+    public static void main(String[] args){
+        int rows=5;
+        //Pattern: Pyramid Pattern
+        System.out.println("Pyramid Pattern:");
+        for(int i=1;i<=rows;i++){
+            for(int j=rows;j>i;j--){
+                System.out.print(" ");
+            }
+            for(int k=1;k<=(2*i-1);k++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        
+    }
+}
