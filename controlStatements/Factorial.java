275add2680e75647320455aef40c7bf7b389812d
Karthik Kumar Bandaru <bandarukarthik24@gmail.com>
2026-01-03 10:34:57 +0530

    Factorial of a number program using java.

---
+++ controlStatements/Factorial.java
@@ -0,0 +1,15 @@
+package controlStatements;
+import java.util.Scanner;
+public class Factorial {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        System.out.print("Enter a positive integer to calculate its factorial: ");
+        int number=scanner.nextInt();
+        int factorial=1;
+        for(int i=1;i<=number;i++){
+            factorial=factorial*i;
+        }
+        System.out.println("Factorial of "+number+" is: "+factorial);
+        scanner.close();
+    }
+}
