db9384f6611752e30650d7bebbafd65d33530c97
Karthik Kumar Bandaru <bandarukarthik24@gmail.com>
2026-01-08 14:16:07 +0530

    Interfaces using java.

---
+++ oops/Interfaces.java
@@ -0,0 +1,24 @@
+interface Camera{
+    void takePhoto();
+}
+
+interface MusicPlayer{
+    void playMusic();
+}
+
+class SmartPhone implements Camera,MusicPlayer{
+    public void takePhoto(){
+        System.out.println("Taking photo using smartphone camera");
+    }
+
+    public void playMusic(){
+        System.out.println("Playing music on smartphone"); 
+    }
+} 
+public class Interfaces {
+    public static void main(String[] args) {
+        SmartPhone phone  = new SmartPhone();
+        phone.takePhoto();
+        phone.playMusic();
+    }
+}
