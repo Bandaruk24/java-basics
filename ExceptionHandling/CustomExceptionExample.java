2340b964331b28d8070dee4a484207eaed591f0b
Karthik Kumar Bandaru <bandarukarthik24@gmail.com>
2026-01-09 14:28:10 +0530

    Custom Eception Example using java.

---
+++ ExceptionHandling/CustomExceptionExample.java
@@ -0,0 +1,25 @@
+package ExceptionHandling;
+class InvalidAgeException extends Exception {
+    InvalidAgeException(String message) {
+        super(message);
+    }
+}
+
+public class CustomExceptionExample {
+
+    static void validateAge(int age) throws InvalidAgeException {
+        if (age < 18) {
+            throw new InvalidAgeException("Age must be 18 or above");
+        } else {
+            System.out.println("Access granted");
+        }
+    }
+
+    public static void main(String[] args) {
+        try {
+            validateAge(15);
+        } catch (InvalidAgeException e) {
+            System.out.println("Exception caught: " + e.getMessage());
+        }
+    }
+}
