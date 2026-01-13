b54e1e994218d57780a79d315fa9649392c34e69
Karthik Kumar Bandaru <bandarukarthik24@gmail.com>
2026-01-13 11:53:06 +0530

    Read file using FileReader.

--- FileHandling/ReadFileUsingFileReader.java
+++ FileHandling/ReadFileUsingFileReader.java
@@ -3,12 +3,19 @@ import java.io.IOException;
 
 class ReadFileUsingFileReader{
     public static void main(String[] args) {
+        // FileReader is a character stream class in Java used to read text files.
         try{
-        FileReader reader = new FileReader("example.txt");
-        int ch;
-        while(( ch = reader.read()) != -1){
-            System.out.print((char) ch);
-        }
+            // Creating a FileReader Object and Opens the file for reading..
+            FileReader reader = new FileReader("example.txt");
+            // initializing a char.
+            int ch;
+            // reader.read() - Reads one character at a time Returns: Character ASCII value 
+            // -1 → End of file (EOF)
+            while(( ch = reader.read()) != -1){
+                // (char) ch : Converts ASCII value into character
+                System.out.print((char) ch);
+            }
+            // Releases system resources (must do)
             reader.close();
         }
         catch(IOException e){
