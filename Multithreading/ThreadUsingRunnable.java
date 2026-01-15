97fcb78742d642f0db963e7fc80b95c18d217075
Karthik Kumar Bandaru <bandarukarthik24@gmail.com>
2026-01-15 21:36:24 +0530

    Thread using Runnable.

--- Multithreading/ThreadUsingRunnable.java
+++ Multithreading/ThreadUsingRunnable.java
@@ -1,5 +1,11 @@
 package Multithreading;
 
+// Steps to Create Thread Using Runnable
+// Create a class that implements Runnable
+// Override the run() method
+// Create a Thread object
+// Pass Runnable object to Thread
+// Call start()
 class ThreadUsingRunnable implements Runnable{
     public void run(){
      for(int i=0;i<=5;i++){
@@ -16,8 +22,13 @@ class ThreadUsingRunnable implements Runnable{
     public static void main(String[] args) {
         Thread t1 = new Thread(new ThreadUsingRunnable());
         Thread t2 = new Thread(new ThreadUsingRunnable());
-       
+        // t.start(); // New thread created
         t1.start();
         t2.start();
     }
+
+    // Why is Runnable preferred? => Supports multiple inheritance & better design
+    // Can we call start() on Runnable? => No, start() belongs to Thread
+    // Can one Runnable be used by multiple threads? => Yes
+    // Can Runnable return value? => No (use Callable)
 }
