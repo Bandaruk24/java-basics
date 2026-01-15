1ca20a4ccad462d315c4a855c352022c3ce06073
Karthik Kumar Bandaru <bandarukarthik24@gmail.com>
2026-01-15 20:38:16 +0530

    Thread using Runnable.

---
+++ Multithreading/ThreadUsingRunnable.java
@@ -0,0 +1,23 @@
+package Multithreading;
+
+class ThreadUsingRunnable implements Runnable{
+    public void run(){
+     for(int i=0;i<=5;i++){
+            System.out.println(Thread.currentThread().getName()+" : "+i);
+        }
+        try{
+            Thread.sleep(500);
+        }
+        catch(InterruptedException e){
+            e.printStackTrace();
+        }
+        
+    } 
+    public static void main(String[] args) {
+        Thread t1 = new Thread(new ThreadUsingRunnable());
+        Thread t2 = new Thread(new ThreadUsingRunnable());
+       
+        t1.start();
+        t2.start();
+    }
+}
