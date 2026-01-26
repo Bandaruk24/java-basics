0934a061d9ffa22dc85dc8b150a2b80e7a14f80a
Karthik Kumar Bandaru <bandarukarthik24@gmail.com>
2026-01-26 17:34:20 +0530

    Lambda Expressions

---
+++ java8features/LambdaExpressions.java
@@ -0,0 +1,21 @@
+package java8features;
+
+import java.util.*;
+
+//A lambda expression is a shorter way to write an implementation of a functional interface.
+@FunctionalInterface
+interface  Calculator {
+    int calculate(int a,int b);
+}
+public class LambdaExpressions {
+    public static void main(String[] args) {
+        Calculator c = (a,b) -> a+b;
+        Calculator c1 = (a,b) -> a*b;
+        System.out.println("Addition of Numbers: "+c.calculate(1,2));
+        System.out.println("Multiplication of Numbers: "+c1.calculate(2,3));
+
+        //List Creation using lambda functions
+        List<String> names = Arrays.asList("Karthik","Kumar","Bandaru");
+        names.forEach(name->System.out.println(name));
+    };
+}
